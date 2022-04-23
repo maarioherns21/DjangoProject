d0b9d0d27fd0bb14a1fe782b1ed58b53766d1275
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2022-04-22 17:05:38 -0700

    first update

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cars/', views.cars_index, name='index'),
+]
