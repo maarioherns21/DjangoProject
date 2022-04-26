19fac544a69d9ac7a0bd4f0018ff7df6e76bfa60
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2022-04-26 13:00:46 -0700

    Add delete and create and update

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from django.urls import reverse
 # Create your models here.
+
 
 class Car(models.Model):
     make = models.CharField(max_length=100)
@@ -8,10 +10,14 @@ class Car(models.Model):
     description = models.CharField(max_length=250)
     year = models.IntegerField()
 
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'car_id': self.id})
 
 
 class Story(models.Model):
     year = models.IntegerField()
     month = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
+
+
     
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,30 @@
 
+from dataclasses import fields
+from pyexpat import model
 from django.shortcuts import render
 
 from .models import Car
 
 from .models import Story
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
+
+
+class CarCreate(CreateView):
+    model = Car
+    fields = '__all__'
+
+
+class CarUpdate(UpdateView):
+    model = Car
+    fields = ['model', 'year', 'description']
+
+
+class CarDelete(DeleteView):
+    model = Car
+    success_url = '/cars/'
+
 
 def home(request):
     return render(request, 'home.html')
@@ -20,6 +39,8 @@ def cars_index(request):
     cars = Car.objects.all()
     return render(request, 'cars/index.html', {'cars': cars})
 
-def cars_details(request, car_id): # path 'cars/<int:cat_id>/' < this is where car_id comes from 
+
+# path 'cars/<int:cat_id>/' < this is where car_id comes from
+def cars_details(request, car_id):
     car = Car.objects.get(id=car_id)
-    return render(request, 'cars/detail.html' , {'car': car})
+    return render(request, 'cars/detail.html', {'car': car})
